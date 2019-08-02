c7cd3455196b7f5037ff18e9d99f652112fe72a1
Salvador Henares Jiménez <salvador.henares@gmail.com>
2019-08-02 15:25:36 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@
 import Firebase from 'firebase';
 
 const firebaseapp = Firebase.initializeApp({
-    apiKey: "AIzaSyDnN6yO7q2OGooret0YOKl9O-Kvp1KEU9Q",
-    authDomain: "creamicontrato.firebaseapp.com",
-    databaseURL: "https://creamicontrato.firebaseio.com",
-    projectId: "creamicontrato",
-    storageBucket: "creamicontrato.appspot.com",
-    messagingSenderId: "8535832926",
-    appId: "1:8535832926:web:c871e98d79628abb"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: ",
+    messagingSenderId: "",
+    appId: ""
 });
 
 export const db = firebaseapp.database();
