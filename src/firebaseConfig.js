ef6641d313cfdd39a1ca62797f66ad2b8ddfccb8
SahJ00 <salvador.henares@gmail.com>
2019-08-02 15:35:00 +0200

    Subida de firebaseConfig poniendolo en .gitignore

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,13 @@
 import Firebase from 'firebase';
 
 const firebaseapp = Firebase.initializeApp({
-
+    apiKey: "AIzaSyDnN6yO7q2OGooret0YOKl9O-Kvp1KEU9Q",
+    authDomain: "creamicontrato.firebaseapp.com",
+    databaseURL: "https://creamicontrato.firebaseio.com",
+    projectId: "creamicontrato",
+    storageBucket: "creamicontrato.appspot.com",
+    messagingSenderId: "8535832926",
+    appId: "1:8535832926:web:c871e98d79628abb"
 });
 
 export const db = firebaseapp.database();
