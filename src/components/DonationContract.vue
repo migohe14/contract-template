b18f9a6e20ad676d7bcdf66ca4024ee3b969783d
migohe14 <migohe14@gmail.com>
2019-08-19 19:27:17 +0200

    donation contract

---
+++ src/components/DonationContract.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="texte_a_afficher" class="texte_inselectionnable">
+		<span class="stabilisation"></span><p style="text-align: center;"><strong>CONTRATO DE</strong> <strong>DONACIÓN</strong></p>
+        <p style="text-align: right;"><br />En <span id="span_id_ciudad_redaccion"  ><span class="variable_vide">________</span></span>, a <span id="span_id_fecha_redaccion"  ><span class="variable_vide">________</span></span></p>
+        <p style="text-align: center;"><br /><br /><strong>REUNIDOS</strong></p>
+        <p><br /><strong>De una parte,</strong> </p>
+        <p><strong><span id="span_id_nombre_donante1"  >Pepe Garcia Martin</span></strong>, mayor de edad, con domicilio en <span id="span_id_domicilio_donante1"  ><span class="variable_vide">________</span></span>, <span id="span_id_doc_donante1"  >DNI/NIF</span> núm. <span id="span_id_numero_donante1"  >54678954Q</span>, y en su propio nombre y representación. </p>
+        <p><br />En adelante, el "<strong>DONANTE</strong>".</p>
+        <p><br /><strong>De otra parte,</strong></p>
+        <p><strong><span id="span_id_nombre_donatario1"  ><span class="variable_vide">________</span></span></strong>, mayor de edad, con domicilio en <span id="span_id_domicilio_donatario1"  ><span class="variable_vide">________</span></span>, <span id="span_id_doc_donatario1"  >DNI/NIF</span> núm. <span id="span_id_numero_donatario1"  ><span class="variable_vide">________</span></span>, y en su propio nombre y representación.</p>
+        <p><br />En adelante, el "<strong>DONATARIO</strong>".</p>
+        <p><br />El DONANTE y el DONATARIO que, en adelante, podrán ser denominados, individualmente, "la Parte" y conjuntamente, "las Partes", reconociéndose capacidad legal suficiente para contratar y obligarse en la representación que actúan y para aceptar una donación, respectivamente, y siendo responsables de la veracidad de sus manifestaciones,</p>
+        <p style="text-align: center;"><br /><br /><strong>EXPONEN</strong></p>
+        <p><br /><strong>I. </strong>Que el DONANTE es propietario y tiene poder de disposición sobre el siguiente  bien mueble, cuyo valor asciende a ________ (<span id="span_id_valor_bien"  ><span class="variable_vide">________</span></span> €):</p>
+        <p style="padding-left: 30px;"><span id="span_id_descripcion_bien"  ><span class="variable_vide">________</span></span></p>
+        <p><br /><strong>II. </strong>Que el bien se encuentra libre de toda carga, gravamen y limitación, y que no existe sobre el mismo contienda judicial de cualquier otra índole.</p>
+        <p><strong><br />III. </strong>Que el DONANTE tiene la intención voluntaria y consciente de donar al DONATARIO el citado bien, y el DONATARIO de aceptar dicha donación, por lo cual formalizan el presente <strong>CONTRATO DE DONACIÓN</strong>, conforme a las siguientes,</p>
+        <p style="text-align: center;"><br /><br /><strong>ESTIPULACIONES</strong></p>
+        <p style="text-align: left;"><strong><br />PRIMERA.- Objeto.</strong></p>
+        <p>El DONANTE otorga libre, voluntaria, consciente, pura y simplemente en favor del DONATARIO donación del siguiente  bien mueble, cuyo valor ha sido señalado en los Expositivos del presente Contrato:</p>
+        <p style="padding-left: 30px;"><span id="span_id_descripcion_bien"  ><span class="variable_vide">________</span></span></p>
+        <p>El DONANTE manifiesta que le quedan bienes suficientes, en propiedad o en usufructo, para vivir en un estado correspondiente a sus circunstancias; es decir, para atender a su decorosa subsistencia. </p>
+        <p><br /><strong>SEGUNDA.- Aceptación.</strong></p>
+        <p>En este acto, el DONATARIO acepta expresamente la donación otorgada a su favor mediante el presente Contrato.</p>
+        <p><br /><strong>TERCERA.- Donación no sujeta a condición. </strong></p>
+        <p>La donación se realiza sin someterse a ninguna condición.</p>
+        <p><br /><strong>CUARTA.- Perfeccionamiento y efectos de la donación.</strong></p>
+        <p>La donación regulada en el presente Contrato se perfecciona y comienza a producir su efecto desde la fecha de firma del presente Contrato, en la cual, de acuerdo a lo previsto en la estipulación segunda, el DONATARIO manifiesta y el DONANTE conoce su aceptación expresa. </p>
+        <p><br /><strong>QUINTA.- Subrogación del donatario y saneamiento. </strong></p>
+        <p>El DONATARIO se subroga en todos los derechos y acciones que en caso de evicción corresponderían al DONANTE. El DONANTE no queda obligado al saneamiento de lo donado por evicción ni por vicios o defectos ocultos, salvo en caso de actuar con mala fe.</p>
+        <p><br /><strong>SEXTA.- Obligaciones del DONATARIO.</strong></p>
+        <p>El DONATARIO se hará cargo desde la fecha de firma del presente Contrato señalada en el encabezamiento de todos los gastos, pagos o impuestos que afecten al objeto de la presente donación, así como de sanearlo, salvo los casos en los que el saneamiento corresponda al DONANTE, y conservarlo con la diligencia propia de su nueva condición, no pudiendo reclamar en ninguno de estos conceptos al DONANTE.</p>
+        <p><br /><strong>SÉPTIMA.- </strong><strong>Revocación de la donación.</strong></p>
+        <p>De conformidad con lo dispuesto en el Código Civil, la donación será revocada a instancia del DONANTE, con los efectos previstos en este mismo texto legal, en caso de: </p>
+        <p style="padding-left: 30px;">a) Supervivencia o superveniencia de hijos del DONANTE.</p>
+        <p style="padding-left: 30px;">b) Ingratitud del DONATARIO, entendida de acuerdo a lo dispuesto en el artículo 648 del Código Civil.</p>
+        <p><br /><strong>OCTAVA</strong><strong>.- </strong><span class="flou"><strong>Reducción 52 85 52258822 225 inoficiosa.</strong></span></p>
+        <p><span class="flou">52 8582 52 852 85 52258822 5285822 8222888285 822 5552282 5 82 588252822 22 28 55288582 585 528 825822 88888, 2825 8255 52558855 5282225252 28 2528258282222 2 822 828 2228228 252888228 22 58852 22522 82258.</span></p>
+        <p><br /><strong>NOVENA</strong><strong>.- Elevación a público.</strong></p>
+        <p>El documento privado de este Contrato será elevado a escritura pública a petición de cualquiera de las Partes, mediante requerimiento fehaciente, ante el Notario o Notaria que dicha Parte designe. La otra Parte se compromete a firmar el instrumento público cuando para ello fuera requerida.</p>
+        <p><br /><strong>DÉCIMA</strong><strong>.- </strong><strong>Gastos e impuestos.</strong></p>
+        <p>Todos los gastos e impuestos derivados del presente Contrato, incluyendo su elevación a público y los que se pudieran derivar de su inscripción registral, serán a cargo exclusivamente del DONATARIO.</p>
+        <p><br /><strong>DECIMOPRIMERA</strong><strong>.- </strong><strong>Notificaciones.</strong></p>
+        <p>Todas las comunicaciones que en virtud de este documento deban o debieren ser enviadas lo serán por un medio fehaciente que deje constancia del momento en que ha sido enviada, a qué dirección ha sido enviada y el momento de su recepción por la otra Parte, y se interpretarán como correctamente enviadas al DONANTE o al DONATARIO, si se dirigen a las direcciones de las mismas que figuran en este Contrato. Cada una de las Partes puede, por medio de notificación escrita enviada a la otra Parte, cambiar la dirección a la que deban mandarse cualesquiera comunicaciones. </p>
+        <p><br /><strong>DECIMOSEGUNDA</strong><strong>.- <span class="flou">22288858822 528885882 2 255885888822. </span></strong></p>
+        <p><span class="flou">5252 8828282 52852882, 2822885822222, 2252 22 8282, 5 85 225258822, 8588522, 82225252258822, 28525, 2588222885, 222858822 2 22528258822 52 2822 82225522 52 52258822 2, 22 2222558, 5 85 52858822 28258828855 22252 858 255228, 8255 82222852 5 85 82288858822 28252285. 255288585522222, 58 825822 88888.</span></p>
+        <p><span class="flou">52 8582 52 822252825885, 5822522885, 822288822 2 52885258822 22 855222 58 82225522, 2 22 52858822 58 2 52588552 528 28822, 858 255228 58525552 852 82 822222552 5 85 255885888822 52 828 85225528 2 5588525828 82222222228 82222522 5 5252852.</span></p>
+        <p><br />Y en prueba de conformidad y aceptación de todo lo establecido, ambas Partes firman en el lugar y fecha señalados en el encabezamiento, a un solo efecto, todas las hojas de este Contrato en dos ejemplares, uno para cada Parte.</p>
+        <p><br /><br /><br /><br /><br /><br /><strong>...........................................</strong><br /><strong>EL DONANTE</strong></p>
+        <p><br /><br /><br /><br /><br /><br /><strong>...........................................</strong><br /><strong>EL DONATARIO</strong></p>
+    </div>
+</template>
